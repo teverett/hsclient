956919f626e7a39be27c7e289c400b44eb7009e1
Tom Everett <tom@khubla.com>
2020-01-05 11:58:14 -0700

    updated javadoc

--- src/main/java/com/khubla/hsclient/HSClient.java
+++ src/main/java/com/khubla/hsclient/HSClient.java
@@ -9,79 +9,79 @@ public interface HSClient extends Closeable {
 	/**
 	 * send control command by device label
 	 *
-	 * @param label
-	 * @param value
+	 * @param label name of value to set
+	 * @param value value to set
 	 * @return Device
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Device controlDeviceByLabel(String label, String value) throws HSClientException;
 
 	/**
 	 * send control command by device ref
 	 *
-	 * @param ref
-	 * @param value
+	 * @param ref reference id of device
+	 * @param value value to set
 	 * @return Device
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Device controlDeviceByValue(Integer ref, String value) throws HSClientException;
 
 	/**
 	 * get value of counter
 	 *
-	 * @param name
+	 * @param name Name of counter
 	 * @return counter
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Counter getCounter(String name) throws HSClientException;
 
 	/**
 	 * get a device by ref
 	 *
-	 * @param ref
+	 * @param ref reference id of device
 	 * @return Device
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Device getDevice(Integer ref) throws HSClientException;
 
 	/**
 	 * get a device with controls, by ref
 	 *
-	 * @param ref
+	 * @param ref reference id of device
 	 * @return Device
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Device getDeviceControls(Integer ref) throws HSClientException;
 
 	/**
 	 * get all devices with controls
 	 *
-	 * @return Map<String, Device>
-	 * @throws HSClientException
+	 * @return Map of devices by device name
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<String, Device> getDeviceControlsByName() throws HSClientException;
 
 	/**
 	 * get all devices by reference id
 	 *
-	 * @return Map<Integer, Device>
-	 * @throws HSClientException
+	 * @return Map of devices by reference number
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<Integer, Device> getDeviceControlsByRef() throws HSClientException;
 
 	/**
 	 * get all devices by name
 	 *
-	 * @return Map<String, Device>
-	 * @throws HSClientException
+	 * @return Map of devices by device name
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<String, Device> getDevicesByName() throws HSClientException;
 
 	/**
 	 * get all devices by reference id
 	 *
-	 * @return Map<Integer, Device>
-	 * @throws HSClientException
+	 * @return Map of devices by reference number
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<Integer, Device> getDevicesByRef() throws HSClientException;
 
@@ -89,7 +89,7 @@ public interface HSClient extends Closeable {
 	 * get all events by id
 	 *
 	 * @return events by id
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<Integer, Event> getEventsById() throws HSClientException;
 
@@ -97,7 +97,7 @@ public interface HSClient extends Closeable {
 	 * get all events by name
 	 *
 	 * @return events by name
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	Map<String, Event> getEventsByName() throws HSClientException;
 
@@ -105,7 +105,7 @@ public interface HSClient extends Closeable {
 	 * get all locations1
 	 *
 	 * @return list of location names
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	List<String> getLocations1() throws HSClientException;
 
@@ -113,7 +113,7 @@ public interface HSClient extends Closeable {
 	 * get all locations2
 	 *
 	 * @return list of location names
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	List<String> getLocations2() throws HSClientException;
 
@@ -121,7 +121,7 @@ public interface HSClient extends Closeable {
 	 * run an event by event id
 	 *
 	 * @param eventId
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	void runEvent(Integer eventId) throws HSClientException;
 
@@ -130,7 +130,7 @@ public interface HSClient extends Closeable {
 	 *
 	 * @param group
 	 * @param name
-	 * @throws HSClientException
+	 * @throws HSClientException Exception encountered communicating with HomeSeer
 	 */
 	void runEvent(String group, String name) throws HSClientException;
 }
